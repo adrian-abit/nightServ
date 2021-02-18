bc3a3f904dd40c5f39e7bf64d00d29a969494319
adrian <adrian@adrianbit.tech>
2021-02-18 21:13:45 +0100

    made some changes to messages and links. made code fancier and faster. added dark mode to calendar and others

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 // stolen function to check if the iserv css files are there
 function isCSSthere() {
     let is = false;
-    console.log(document.styleSheets.length + " + " + document.styleSheets.length == 0)
     if (document.styleSheets.length == 0) is = null;
     for (var i = 0; i < document.styleSheets.length; i++) {
         if (document.styleSheets[i].href !== null) {
@@ -26,63 +25,75 @@ function docReady(fn) {
 }
 
 
-
 //check if the website is cached as an iserv site
-chrome.runtime.sendMessage("getData", r => {
+let url = extractDomain(location.href);
 
-    let url = r.url;
+chrome.storage.local.get([url, "nightServ_enabled"], res => {
 
-    chrome.storage.local.get([url, "nightServ_enabled"], res => {
-        let inject = false;
+    if (res[url] == null)
+        docReady(() => {
+            let rss = {};
+            let ic = isCSSthere();
+            if (ic != null) {
+                rss[url] = ic;
+                chrome.storage.local.set(rss);
+                location.reload();
+            }
+        });
 
-        if (!res["nightServ_enabled"]) inject = false;
-        else if (res[url] == null)
-            docReady(() => {
-                let rss = {};
-                let ic = isCSSthere();
-                if (ic != null) {
-                    rss[url] = ic;
-                    chrome.storage.local.set(rss);
-                    inject = rss[url];
-                    location.reload();
+
+    if (res[url] && res["nightServ_enabled"]) {
+        //chage some things in the page
+        docReady(() => {
+
+            //replace iserv logo
+            let uri = chrome.runtime.getURL("nightservfull.svg");
+            let el = document.getElementById("sidebar-nav-header");
+            if (el != null) {
+                let img = el.firstChild.nextElementSibling.firstChild.nextElementSibling.firstChild.nextElementSibling;
+                img.src = uri;
+                img.srcset = uri;
+                img.width = 125;
+            }
+
+            //change school text
+            el = document.getElementsByClassName("brand");
+            if (el.length > 0) {
+                for (let i = 0; i < el.length; i++) {
+                    let manifest = chrome.runtime.getManifest();
+                    el[i].textContent = manifest.name + " v" + manifest.version_name;
                 }
-            });
-        else if (res[url]) inject = true;
+            }
+
+            //insert feedback option
+            el = document.getElementById("menu-wrapper");
+            if (el != null && el != undefined) {
+                let button = document.createElement("li");
+                button.setAttribute("class", "nav-item nav-module menu-item-nightserv");
+
+                let buttoncontent = document.createElement("a");
+                buttoncontent.setAttribute("href", "https://nightserv.abit.dev/feedback");
+
+                let buttonimg = document.createElement("img");
+                buttonimg.setAttribute("class", "nav-svg-icon");
+                buttonimg.setAttribute("src", chrome.runtime.getURL("N.svg"));
+                buttoncontent.appendChild(buttonimg);
+
+                let buttonlabel = document.createElement("span");
+                buttonlabel.setAttribute("class", "item-label");
+                buttonlabel.appendChild(document.createTextNode("nightServ Feedback"))
+                buttoncontent.appendChild(buttonlabel);
+
+                button.appendChild(buttoncontent);
+                el.appendChild(button)
+            }
+        });
+
+    }
+})
 
 
-        if (inject) {
-            //inject css into page
-            chrome.storage.local.get("nightCSS", res => {
-                const style = document.createElement('style');
-                style.textContent = res["nightCSS"];
-                document.head.append(style);
-            })
-
-            //chage some things in the page
-            docReady(() => {
-
-                //replace iserv logo
-                let uri = chrome.runtime.getURL("nightservfull.svg");
-                let el = document.getElementById("sidebar-nav-header");
-                if (el != null) {
-                    let img = el.firstChild.nextElementSibling.firstChild.nextElementSibling.firstChild.nextElementSibling;
-                    img.src = uri;
-                    img.srcset = uri;
-                    img.width = 125;
-                }
-
-                //change school text
-                el = document.getElementsByClassName("brand");
-                if (el.length > 0) {
-                    for (let i = 0; i < el.length; i++) {
-                        let manifest = chrome.runtime.getManifest();
-                        el[i].textContent = manifest.name + " v" + manifest.version_name;
-                    }
-                }
-
-            });
-
-        }
-    })
-});
+function extractDomain(url) {
+    return url.replace(/^(?:https?:\/\/)?(?:[^\/]+\.)?([^.\/]+\.[^.\/]+).*$/, "$1");
+}
 
--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 let enabled = true;
+let verydark = false;
 
 document.getElementById("abit").addEventListener("click", () => {
   window.open(
-    "https://nightserv.adrianbit.tech/",
+    "https://nightserv.abit.dev/",
     "_blank"
   );
 });
@@ -10,11 +11,27 @@ document.getElementById("abit").addEventListener("click", () => {
 document.getElementById("changeNight").addEventListener("click", () => {
   enabled = !enabled;
   chrome.storage.local.set({ "nightServ_enabled": enabled });
-  chrome.storage.local.get("nightServ_enabled", res => console.log(res));
   changeText();
 });
 
+document.getElementById("changeVD").addEventListener("click", () => {
+  if (!verydark) {
+    verydark = true;
+    chrome.storage.local.set({ "verydark": verydark });
+    changeText();
+  }
+});
+
+document.getElementById("changeD").addEventListener("click", () => {
+  if (verydark) {
+    verydark = false;
+    chrome.storage.local.set({ "verydark": verydark });
+    changeText();
+  }
+});
+
 function changeText() {
+  console.log(enabled);
   if (enabled) {
     document.getElementById("changeNight").innerHTML = "<span class='disable'>Nightmode Ausschalten</span>";
     document.getElementById("changeNight").style.border = "1.5px solid #F44336";
@@ -23,13 +40,28 @@ function changeText() {
     document.getElementById("changeNight").style.border = "1.5px solid #4CAF50";
   }
 
+  if (verydark) {
+    document.getElementById("changeVD").style.border = "1.5px solid #8AB4F8";
+    document.getElementById("changeD").style.border = "1.5px solid #5F6368";
+  } else {
+    document.getElementById("changeD").style.border = "1.5px solid #8AB4F8";
+    document.getElementById("changeVD").style.border = "1.5px solid #5F6368";
+  }
+
 }
 
-chrome.storage.local.get(["nightServ_enabled"], res => {
-  if (isEmpty(res))
+
+chrome.storage.local.get(["nightServ_enabled", "verydark"], res => {
+  console.log(res);
+  if (isEmpty(res) || res["nightServ_enabled"] == null)
     chrome.storage.local.set({ "nightServ_enabled": true });
-  else if (!res["nightServ_enabled"])
-    enabled = false;
+  else
+    enabled = res["nightServ_enabled"];
+
+  if (isEmpty(res) || res["verydark"] == null)
+    chrome.storage.local.set({ "verydark": false });
+  else
+    verydark = res["verydark"];
 
   changeText();
 })
